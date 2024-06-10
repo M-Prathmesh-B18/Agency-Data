c0f787c6cd9299e013bfeb86445c981c8dd004af
M Prathmesh <prathameshmahangare369@gmail.com>
2024-06-10 20:59:39 +0530

    in UserDataAgency class we creating a data base for insert data n sql

--- Gas-Agency-UserData/src/main/java/com/agency/UserDataEntry.java
+++ Gas-Agency-UserData/src/main/java/com/agency/UserDataEntry.java
@@ -1,6 +1,8 @@
 package com.agency;
 
 import java.io.IOException;
+
+
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
@@ -8,34 +10,37 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.sql.*;
 
-/**
- * Servlet implementation class UserData
- */
+ 
 @WebServlet("/UserData")
-public class UserData extends HttpServlet {
+public class UserDataEntry extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	
+	public void init()
+	{
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/GasAgencyDB","root","mprathamsql1810");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public UserData() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+    
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+	 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 	   PrintWriter pw=response.getWriter();
 	   pw.println("hello");
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
