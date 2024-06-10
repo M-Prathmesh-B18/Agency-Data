72d27ee0ee14769456626816637f426c0270f489
M Prathmesh <prathameshmahangare369@gmail.com>
2024-06-10 20:47:56 +0530

    creating the pojo class for creating the object of the user

---
+++ Gas-Agency-UserData/src/main/java/com/pojo/UserPojo.java
@@ -0,0 +1,40 @@
+package com.pojo;
+
+public class UserPojo {
+   private String registerId;
+   private String uname;
+   private String mobile;
+   private boolean cash;
+   private boolean online;
+public String getRegisterId() {
+	return registerId;
+}
+public void setRegisterId(String registerId) {
+	this.registerId = registerId;
+}
+public String getUname() {
+	return uname;
+}
+public void setUname(String uname) {
+	this.uname = uname;
+}
+public String getMobile() {
+	return mobile;
+}
+public void setMobile(String mobile) {
+	this.mobile = mobile;
+}
+public boolean isCash() {
+	return cash;
+}
+public void setCash(boolean cash) {
+	this.cash = cash;
+}
+public boolean isOnline() {
+	return online;
+}
+public void setOnline(boolean online) {
+	this.online = online;
+}
+   
+}
